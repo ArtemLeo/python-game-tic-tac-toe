96fd729c5cf3df1ec741d75d411096ae07c63f66
Artem Andriushchenko <link.by.artem@gmail.com>
2024-07-04 07:23:17 +0300

    Changed the font and font size options

--- runner.py
+++ runner.py
@@ -5,21 +5,29 @@ import time
 import tictactoe as ttt
 
 pygame.init()
-size = width, height = 600, 400
+size = width, height = 750, 550
 
 # Colors
 black = (0, 0, 0)
 white = (255, 255, 255)
+yellow = (255, 255, 0)
+blue = (60, 0, 60)
+red = (100, 0, 0)
 
 screen = pygame.display.set_mode(size)
 
-mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
-largeFont = pygame.font.Font("OpenSans-Regular.ttf", 40)
-moveFont = pygame.font.Font("OpenSans-Regular.ttf", 60)
+smallFont = pygame.font.Font("Writterine-Regular.ttf", 25)
+middleFont = pygame.font.Font("Writterine-Regular.ttf", 28)
+bigFont = pygame.font.Font("Writterine-Regular.ttf", 50)
+largeFont = pygame.font.Font("Writterine-Regular.ttf", 85)
+moveFont = pygame.font.Font("Writterine-Regular.ttf", 110)
 
 user = None
 board = ttt.initial_state()
 ai_turn = False
+
+# Завантаження зображення фону
+background_image = pygame.image.load("background_image.jpg").convert()
 
 while True:
 
@@ -27,30 +35,35 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    screen.fill(black)
+    # Відображення зображення фону
+    screen.blit(background_image, (0, 0))
 
     # Let user choose a player.
     if user is None:
 
         # Draw title
-        title = largeFont.render("Play Tic-Tac-Toe", True, white)
+        title = largeFont.render("Play Tic-Tac-Toe", True, black)
+        title2 = smallFont.render("https://github.com/ArtemLeo/python-game-tic-tac-toe", True, black)
         titleRect = title.get_rect()
-        titleRect.center = ((width / 2), 50)
+        titleRect2 = title.get_rect()
+        titleRect.center = ((width / 2), 180)
+        titleRect2.center = ((width / 2), 270)
         screen.blit(title, titleRect)
+        screen.blit(title2, titleRect2)
 
         # Draw buttons
-        playXButton = pygame.Rect((width / 8), (height / 2), width / 4, 50)
-        playX = mediumFont.render("Play as X", True, black)
+        playXButton = pygame.Rect((width / 5), (height / 2) + 100, width / 5, 50)
+        playX = middleFont.render("Play as X", True, white)
         playXRect = playX.get_rect()
         playXRect.center = playXButton.center
-        pygame.draw.rect(screen, white, playXButton)
+        pygame.draw.rect(screen, black, playXButton)
         screen.blit(playX, playXRect)
 
-        playOButton = pygame.Rect(5 * (width / 8), (height / 2), width / 4, 50)
-        playO = mediumFont.render("Play as O", True, black)
+        playOButton = pygame.Rect(5 * (width / 8), (height / 2) + 100, width / 5, 50)
+        playO = middleFont.render("Play as O", True, white)
         playORect = playO.get_rect()
         playORect.center = playOButton.center
-        pygame.draw.rect(screen, white, playOButton)
+        pygame.draw.rect(screen, black, playOButton)
         screen.blit(playO, playORect)
 
         # Check if button is clicked
@@ -67,7 +80,7 @@ while True:
     else:
 
         # Draw game board
-        tile_size = 80
+        tile_size = 130
         tile_origin = (width / 2 - (1.5 * tile_size),
                        height / 2 - (1.5 * tile_size))
         tiles = []
@@ -79,10 +92,10 @@ while True:
                     tile_origin[1] + i * tile_size,
                     tile_size, tile_size
                 )
-                pygame.draw.rect(screen, white, rect, 3)
+                pygame.draw.rect(screen, white, rect, 2)
 
                 if board[i][j] != ttt.EMPTY:
-                    move = moveFont.render(board[i][j], True, white)
+                    move = moveFont.render(board[i][j], True, yellow)
                     moveRect = move.get_rect()
                     moveRect.center = rect.center
                     screen.blit(move, moveRect)
@@ -103,9 +116,9 @@ while True:
             title = f"Play as {user}"
         else:
             title = f"Computer thinking..."
-        title = largeFont.render(title, True, white)
+        title = bigFont.render(title, True, black)
         titleRect = title.get_rect()
-        titleRect.center = ((width / 2), 30)
+        titleRect.center = ((width / 2), 40)
         screen.blit(title, titleRect)
 
         # Check for AI move
@@ -124,15 +137,15 @@ while True:
             mouse = pygame.mouse.get_pos()
             for i in range(3):
                 for j in range(3):
-                    if (board[i][j] == ttt.EMPTY and tiles[i][j].collidepoint(mouse)):
+                    if board[i][j] == ttt.EMPTY and tiles[i][j].collidepoint(mouse):
                         board = ttt.result(board, (i, j))
 
         if game_over:
-            againButton = pygame.Rect(width / 3, height - 65, width / 3, 50)
-            again = mediumFont.render("Play Again", True, black)
+            againButton = pygame.Rect(width / 3, height - 65, width / 3, 45)
+            again = middleFont.render("Play Again ...", True, white)
             againRect = again.get_rect()
             againRect.center = againButton.center
-            pygame.draw.rect(screen, white, againButton)
+            pygame.draw.rect(screen, black, againButton)
             screen.blit(again, againRect)
             click, _, _ = pygame.mouse.get_pressed()
             if click == 1:
